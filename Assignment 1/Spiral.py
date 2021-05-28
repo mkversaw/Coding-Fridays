6c49504cf011fc53af18f59d8bb910985d239d18
Miles Versaw <mkversaw@gmail.com>
2021-05-28 17:03:57 -0500

    First commit

---
+++ Assignment 1/Spiral.py
@@ -0,0 +1,59 @@
+import sys
+import fileinput
+
+
+
+def main():   
+    # open file w/ name spiral.in
+    try:
+       
+        #file = open(fileName) # open file then read lines
+        lines = sys.stdin.readlines()
+        spiral = []
+        
+        for line in lines:
+            str_line = line.rstrip() # strip any new lines
+            int_line = int(str_line) # insert elements as INTEGERS
+            spiral.append(int_line)
+        
+        size = spiral[0]
+        
+        
+        spiral2D = create_spiral(size)
+        
+        print_list(spiral)
+        #file.close()
+
+    
+    except FileNotFoundError:
+        print("Failed to open file, did not exist")
+    except Exception as e:
+        print("Program failed: ", e)
+    else:
+        print("Program finished successfully")
+
+
+# Input: n is an odd integer between 1 and 100
+# Output: returns a 2-D list representing a spiral
+#         if n is even add one to n
+def create_spiral ( n ):
+    if(n < 1):
+        raise ValueError("Given spiral size is invalid (less than 1)")
+    if( (n % 2) != 0 ):
+        n += 1
+    
+    
+    
+# Input: spiral is a 2-D list and n is an integer
+# Output: returns an integer that is the sum of the
+#         numbers adjacent to n in the spiral
+#         if n is outside the range return 0
+def sum_adjacent_numbers (spiral, n):
+    pass
+
+def print_list (list):
+    for i in list:
+        print(i)
+
+if __name__ == "__main__":
+    main()
